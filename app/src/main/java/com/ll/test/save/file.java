dae7c28e439f6e1b5b9c26c2b7f8faad7ad7ac0f
llbug13 <215404264@qq.com>
2016-08-30 18:02:16 +0800

    ll

--- app/src/main/java/com/ll/test/save/file.java
+++ app/src/main/java/com/ll/test/save/file.java
@@ -1,5 +1,7 @@
 package com.ll.test.save;
 
+import android.os.Environment;
+
 import com.ll.test.app.app;
 
 import java.io.File;
@@ -12,5 +14,8 @@ public class file {
         File file = app.getContext().getDir("", 1);
 //        返回外部的根目录
         File file1=app.getContext().getExternalCacheDir();
+//        Environment.getDataDirectory()
+//        /Android/data/[package]/file 自己应用的目录
+        Environment.getExternalStorageDirectory().getAbsolutePath();
     }
 }
