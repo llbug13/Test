ad2b8fc7235e0d3f0f8c6fcb3d7b300132af4a36
llbug13 <215404264@qq.com>
2016-09-22 14:23:40 +0800

    ll

---
+++ app/src/main/java/com/ll/test/ndk/NDKTest.java
@@ -0,0 +1,14 @@
+package com.ll.test.ndk;
+
+import com.ll.test.base.BActivity;
+
+/**
+ * Created by LL on 2016/9/21.
+ */
+public class NDKTest extends BActivity {
+    /**
+     * Native method implemented in C/C++
+     */
+    public native void computeFoo();
+
+}
